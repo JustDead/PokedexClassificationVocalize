2c6951f5d29aa3ab10940b76ef3dad3e4e1ce8a7
Loïc Blet <loic.blet.49@gmail.com>
2021-09-03 14:22:45 +0200

    Added Title Screen (main activity)

---
+++ Android/app/src/main/java/com/blet/loic/pokedexvocalize/TitleScreenActivity.java
@@ -0,0 +1,14 @@
+package com.blet.loic.pokedexvocalize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
